911f07bced619616dfbcc92c5d31961f49f8339e
Xein Deniel <xeindeniel@gmail.com>
2025-07-03 20:37:25 +0800

    Fix: cursorline highlight combine

--- lua/oil-git.lua
+++ lua/oil-git.lua
@@ -156,6 +156,7 @@ local function apply_git_highlights()
 					vim.api.nvim_buf_set_extmark(bufnr, ns_id, i - 1, 0, {
 						virt_text = { { " " .. symbol, hl_group } },
 						virt_text_pos = "eol",
+						hl_mode = "combine",
 					})
 				end
 			end
